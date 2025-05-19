d0df927cba839184f63a87d6c211d8e670033c59
Chris Barlow <cgbarlow@gmail.com>
2025-05-19 01:01:57 +0000

    a couple of steps further... still rate limited, will come back later

--- nodejs_projects/core/dist/index.js
+++ nodejs_projects/core/dist/index.js
@@ -1,4 +1,7 @@
 "use strict";
 // Main entry point for the @pubmd/core package
 // Exports will be added here as modules are developed/refactored.
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.corePackageMessage = void 0;
+exports.corePackageMessage = "Hello from @pubmd/core!";
 //# sourceMappingURL=index.js.map
--- nodejs_projects/core/src/index.ts
+++ nodejs_projects/core/src/index.ts
@@ -1,2 +1,4 @@
 // Main entry point for the @pubmd/core package
 // Exports will be added here as modules are developed/refactored.
+
+export const corePackageMessage: string = "Hello from @pubmd/core!";
